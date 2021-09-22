20fc83760b76b7d241cb41de1010d8971580fa35
unknown <vivekumar1995@gmail.com>
2021-09-22 17:10:43 +0530

    checking

---
+++ src/main/java/maven/maven123/App.java
@@ -0,0 +1,13 @@
+package maven.maven123;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
